f32feaf77cda5dccc4cf036739523e0da1100f85
Teetuch <teetuch_palm@hotmail.com>
2023-01-06 13:19:52 +0700

    add middleware to get db and create post handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,45 @@
 package handler
 
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
+)
+
 type Expense struct {
 	ID int
 	Title string
 	Amount int
 	Note string
 	Tags []string
+}
+
+var db *sql.DB
+
+func GetDB(dbsever *sql.DB) echo.MiddlewareFunc {
+    return func(next echo.HandlerFunc) echo.HandlerFunc {
+        return func(c echo.Context) error {
+			db = dbsever
+            return next(c)
+            
+        }
+    }
+}
+
+func CreateExpensesHandler(c echo.Context) error {
+	ex := Expense{}
+	err := c.Bind(&ex) //เเปลงให้เป็น byte
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	row := db.QueryRow("INSERT INTO expenses (title, amount, note, tags) values ($1, $2, $3, $4)  RETURNING id", ex.Title, ex.Amount, ex.Note, pq.Array(&ex.Tags))
+	err = row.Scan(&ex.ID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusCreated, ex)
 }
