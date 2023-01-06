0876a4041388e0974c0c70727d927811dbe55d3a
Teetuch <teetuch_palm@hotmail.com>
2023-01-06 12:47:29 +0700

    init go mod and connect Database

---
+++ Database/DB.go
@@ -0,0 +1,49 @@
+package Database
+
+import (
+	"database/sql"
+	"log"
+)
+
+type Expense struct {
+	ID int
+	Title string
+	Amount int
+	Note string
+	Tags []string
+}
+
+var db *sql.DB
+
+func InitDB(urlDB string) *sql.DB{
+	var err error
+	//urlDB = "postgres://grzuanbs:J_Q4hKYnrgJmSBu8UnJPoxK85vmGhgLq@john.db.elephantsql.com/grzuanbs"
+	db, err = sql.Open("postgres", urlDB)
+	//db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	//defer db.Close()
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);
+	`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+ 
+	return db
+
+}
+
+
+
+
--- server.go
+++ server.go
@@ -3,9 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"database/sql"
+	"log"
+	"apiExpense/Database"
+
 )
 
+
+var db *sql.DB
+
 func main() {
+
+	db = Database.InitDB("postgres://grzuanbs:J_Q4hKYnrgJmSBu8UnJPoxK85vmGhgLq@john.db.elephantsql.com/grzuanbs")
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+	log.Fatal(e.Start(":2565"))
 }
